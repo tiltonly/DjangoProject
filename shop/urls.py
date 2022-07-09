469acf67808d9c30107d3b3e9ffeb5c690c655df
Maxim Gvozdeev <gvozdeev61@gmail.com>
2022-07-09 12:30:14 +0600

    DjangoProject

---
+++ shop/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+from .views import SearchResultsView
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('contacts', views.contacts, name='contacts'),
+    path('category/<slug:category_slug>', views.home, name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
+    path('cart', views.cart_detail, name='cart_detail'),
+    path('cart/add/<int:product_id>', views.add_cart, name='add_cart'),
+    path('cart/remove/<int:product_id>', views.cart_remove, name='cart_remove'),
+    path('cart/remove_product/<int:product_id>', views.cart_remove_product, name='cart_remove_product'),
+    path('account/create/', views.signUpView, name='signup'),
+    path('account/login/', views.loginView, name='login'),
+    path('account/logout/', views.logoutView, name='logout'),
+    path('search/', SearchResultsView.as_view(), name='search'),
+]
+
